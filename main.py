4d476f9843fc3153c4c73b8be62c231fa19f4082
Scott Collins <scottjcollins19@gmail.com>
2026-05-14 20:58:24 +0100

    Updated data collection class naming

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.atmosphere import Atmosphere
 from src.orbit import Orbit
 from src.monte_carlo import MonteCarlo
-from src.data_collection import Machine
+from src.data_collection import Data
 
 
 def main():
